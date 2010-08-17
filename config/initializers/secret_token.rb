3f07f1c6ede43824fa5564d12c8085ce13ca4fc7
Michael Klett <michael@webadvocate.com>
2010-08-17 13:35:56 -0400

    Bare app with doculab added to Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DocsChargifyCom::Application.config.secret_token = '8f05d02fdd2332c96c7ee1020500d73335cddb8d1b1fb631b9b2054284617574afd5a39fff261559cf217bdd24fec696058ab148f73c86659b201e12d0e8d6ec'
