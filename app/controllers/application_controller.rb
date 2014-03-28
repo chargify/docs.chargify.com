6025655c1fcc256779ffcccaf517ebb449093af7
Drew Blas <drew.blas@gmail.com>
2014-03-28 12:00:37 -0500

    Added domain and SSL enforcement in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :enforce_domain
+
+  def enforce_domain
+    if Rails.env.production?
+      if !request.ssl? || request.host != 'docs.chargify.io'
+        redirect_to({:protocol => 'https://', :host => 'docs.chargify.io'}.merge(params), :status => 301)
+      end
+    end
+  end
 end
